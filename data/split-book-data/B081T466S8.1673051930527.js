93e7dbcafbdad9f15112412dab70bb1534206d5d
bksnetwork <96927177+bksnetwork@users.noreply.github.com>
2023-01-06 19:41:20 -0500

    /
    /

---
+++ data/split-book-data/B081T466S8.1673051930527.js
@@ -0,0 +1,1 @@
+window.bookSummaryJSON = "<p>Stella's life is upset when she moves to Nebraska to live with her mom after her dad's death. She realizes, at Mysterium Excelsum, everyone has a certain magic to them. Students go missing in the school and she must find out why...before someone dies.</p>"; 
